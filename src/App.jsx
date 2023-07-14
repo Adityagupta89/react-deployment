04c08566f2524fc055fb4bb1e48e4d75d66384df
Aditya <aditya.gupta@thinkitive.com>
2023-07-15 04:04:38 +0530

    feat : add the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <h1>My First React App with aws deployment with version 2</h1>
-  )
-}
+    <>
+      <h1>My First React App with aws deployment with version 2</h1>
+      <p>finally I add the cicd pipeline </p>
+    </>
+  );
+};
 
-export default App
+export default App;
